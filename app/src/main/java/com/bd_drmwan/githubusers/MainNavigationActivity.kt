27770e88336eddd5b0541a593a99aeb01aab84b5
Budi Darmawan <budi20.darmawan20@gmail.com>
2021-10-10 14:17:28 +0700

    init commit

---
+++ app/src/main/java/com/bd_drmwan/githubusers/MainNavigationActivity.kt
@@ -0,0 +1,11 @@
+package com.bd_drmwan.githubusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_navigation)
+    }
+}
